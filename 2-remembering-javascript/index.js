e9dd282c90684a027efee507464acea51cede6e7
AlexNuclear <peaceatom@bk.ru>
2018-09-10 22:14:50 +0300

    done
    Кривенько,но done

---
+++ 2-remembering-javascript/index.js
@@ -0,0 +1,36 @@
+
+// -------------------------------
+let numbers = process.argv.slice(2);
+console.log(numbers);
+
+// for ( i=0;i<= numbers.lenght;i++) {
+//   Number(numbers[i]);
+// }
+let numbersSum = parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4]);
+console.log('Сумма: ' + numbersSum);
+
+let numberMiddle = (parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4])) / numbers.length;
+console.log('Среднее арифметическое:' + numberMiddle);
+
+// -----------------------------------
+
+
+
+// Узнать,почему не сработал перебор массива
+
+// --------------------------------------------------------- //
+
+// Неудачные дубли
+
+// let numbers = process.argv.slice(2);
+// console.log(numbers);
+//
+// for ( i=1;i<= numbers.lenght;i++) {
+//   var numbersToNumbers = [];
+//   numbersToNumbers.push( Number(numbers[i]))
+// }
+//
+// console.log(numbersToNumbers);
+
+// var numberSumm = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+// console.log(numberSumm);
