e1d8141ce02911c7c66af19b687416a165de6f00
AlexNuclear <peaceatom@bk.ru>
2018-09-10 23:21:42 +0300

    done 2
    Сделал по-человечески

--- 2-remembering-javascript/index.js
+++ 2-remembering-javascript/index.js
@@ -1,23 +1,51 @@
 
 // -------------------------------
-let numbers = process.argv.slice(2);
-console.log(numbers);
+// let numbers = process.argv.slice(2);
+// console.log(numbers);
 
 // for ( i=0;i<= numbers.lenght;i++) {
 //   Number(numbers[i]);
 // }
-let numbersSum = parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4]);
-console.log('Сумма: ' + numbersSum);
-
-let numberMiddle = (parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4])) / numbers.length;
-console.log('Среднее арифметическое:' + numberMiddle);
+// let numbersSum = parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4]);
+// console.log('Сумма: ' + numbersSum);
+//
+// let numberMiddle = (parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]) + parseInt(numbers[3]) + parseInt(numbers[4])) / numbers.length;
+// console.log('Среднее арифметическое:' + numberMiddle);
 
 // -----------------------------------
 
+// Вариант 1 - уродливый
+
+// Узнать,почему не сработал перебор массива и последующее приведение типов к цифре
 
 
-// Узнать,почему не сработал перебор массива
 
+// Разобрался в массиве!
+
+let numbers = process.argv.slice(2);
+console.log(numbers);
+
+let numbersSumm = 0;
+
+for (i=0; i<numbers.length;i++) {
+
+    numbersSumm = numbersSumm + parseInt(numbers[i], 10);
+}
+console.log('Сумма:', numbersSumm);
+
+console.log('Среднее арифметическое:' + (numbersSumm / numbers.length));
+//---------------------------
+
+//
+// let numbersSumm = 0;
+//
+// for (i=0; i<numbers.length; i++) {
+//   numberSumm = numberSumm + parseInt(numbers[i],10);
+// }
+// console.log(numberSumm);
+
+
+//----------------------------------
 // --------------------------------------------------------- //
 
 // Неудачные дубли
